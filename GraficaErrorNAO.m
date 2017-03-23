39c97886f87b8c60245e2c3eeb9a0ac973d0b192
Levid Rodriguez <levid.rodriguez@gmail.com>
2017-03-22 23:25:58 -0600

    no message

--- GraficaErrorNAO.m
+++ GraficaErrorNAO.m
@@ -17,21 +17,21 @@ p_Real = polyval(P_Real, ID);
 %plot(ID,p_Real);
 %plot(ID,Error); 
 hold off
-title('Error en Distancias')
-xlabel('Prueba')
-ylabel('Distancia')
+title('Error en Distancias', 'FontSize',20,'FontWeight','bold','Color','k')
+xlabel('Prueba', 'FontSize',15,'FontWeight','bold','Color','k')
+ylabel('Distancia','FontSize',15,'FontWeight','bold','Color','k')
 xlim([-1 10])
 ylim([-5 100])
 
 
 figure;
-plot(Ideal, Desp); 
+plot(Ideal, Desp,'--b','LineWidth',2,'markersize',10,'markerfacecolor','b'); 
 grid minor
-title('Error en Desplazamiento')
-xlabel('Prueba')
-ylabel('Distancia')
-xlim([-1 10])
-ylim([-5 100])
+title('Error en Desplazamiento', 'FontSize',20,'FontWeight','bold','Color','k')
+xlabel('Prueba','FontSize',15,'FontWeight','bold','Color','k')
+ylabel('Distancia','FontSize',15,'FontWeight','bold','Color','k')
+xlim([-5 100])
+ylim([-1 10])
 
 % y =ax+b
 % y_ideal = 10X
