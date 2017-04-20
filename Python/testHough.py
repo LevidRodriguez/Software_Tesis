cc968ba04043d1f8675f5ad184bdcb3c8c14ecd7
Levid Rodriguez <levid.rodriguez@gmail.com>
2017-04-19 22:55:41 -0500

    no message

--- Python/testHough.py
+++ Python/testHough.py
@@ -5,7 +5,10 @@ import cv2
 def main():
     print "Este es el main..."
     print "OpenCV Version: " + cv2.__version__
-
+    img = cv2.imread("Imagen14.png")
+    cv2.imshow("Imagen De Entrada",img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
     pass
 
 
