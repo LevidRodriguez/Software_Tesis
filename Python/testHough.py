18f26c45a06e0393fc46bfd172b21916ae33ab88
Levid Rodriguez <levid.rodriguez@gmail.com>
2017-04-20 00:30:41 -0500

    no message

--- Python/testHough.py
+++ Python/testHough.py
@@ -5,8 +5,13 @@ import cv2
 def main():
     print "Este es el main..."
     print "OpenCV Version: " + cv2.__version__
-    img = cv2.imread("Imagen14.png")
-    cv2.imshow("Imagen De Entrada",img)
+    img = cv2.imread("../ImagenesTest/Imagen14.png")
+    cv2.imshow("Imagen De Entrada", img)
+    gray = cv2.cvtColor(img, cv2.COLOR_BAYER_RG2GRAY)
+    edges = cv2.Canny(gray, 150, 200, apertureSize=3)
+    lines = cv2.HoughLines(edges, 1, np.pi/180, 200)
+    
+
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     pass
